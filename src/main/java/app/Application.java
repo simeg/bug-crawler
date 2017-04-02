985a3f50bd3a07842ce0d6b50ea391e340ee7676
Simon Egersand <s.egersand@gmail.com>
2017-04-02 18:27:13 +0200

    Crawl URL from queue in separate thread

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,108 +1,50 @@
 package app;
 
-import org.springframework.boot.SpringApplication;
+import app.crawl.Crawler;
+import app.queue.Queue;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.io.*;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
-import java.util.concurrent.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
+import java.io.IOException;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @SpringBootApplication(scanBasePackages={"app"})
 public class Application {
 
-  public static void main(String[] args) {
-    Application app = new Application();
-    app.start();
-    SpringApplication.run(app.getClass(), args);
+  private static final Logger LOG = Logger.getLogger(Application.class.getName());
+
+  public static void main(String[] args) throws IOException {
+    /*
+     * Init by creating three queues:
+     * - subLinkQueue       (URLs to be crawled)
+     * - crawledLinkQueue   (URLs to be analyzed)
+     * - Bugs
+     */
+
+    final Queue subLinkQueue = new Queue();
+    final Queue crawledLinkQueue = new Queue();
+
+    subLinkQueue.add("http://www.vecka.nu");
+
+    // QUESTION: How to control this? Concurrency issue? Prove problem by using debugger over this block
+//    while (subLinkQueue.peek() != null) {
+
+    new Thread(() -> {
+      LOG.log(Level.INFO, "Starting thread with name: {0}", Thread.currentThread().getName());
+
+      // Crawler should add sub-links to subLinkQueue?
+      // And put current URL on crawledLinkQueue
+      Crawler crawler = new Crawler();
+      String url = subLinkQueue.remove().toString();
+      Set<String> subLinks = crawler.getSubLinks(url);
+      
+    }).start();
+
+//    }
+
+//    Application app = new Application();
+//    app.start();
+//    SpringApplication.run(app.getClass(), args);
   }
-
-  private void start() {
-    ScheduledExecutorService scheduledExecutorService =
-        Executors.newScheduledThreadPool(1);
-    ScheduledFuture scheduledFuture =
-        scheduledExecutorService.scheduleWithFixedDelay(
-            // Can be Runnable or lambda. Can lambda replace any class?
-            () -> {
-              String content = readFromUrl("http://vecka.nu/");
-              System.out.println("#####################################");
-              System.out.println(parseWeekNumber(content));
-              System.out.println("#####################################");
-            },
-            0,
-            5,
-            TimeUnit.SECONDS
-        );
-  }
-
-  private String readFromUrl(String theURL) {
-    URL u;
-    InputStream is = null;
-    DataInputStream dis;
-    String s;
-    StringBuffer sb = new StringBuffer();
-
-    try {
-      u = new URL(theURL);
-      is = u.openStream();
-      dis = new DataInputStream(new BufferedInputStream(is));
-      String line;
-      while ((s = dis.readLine()) != null) {
-        line = s + "\n";
-        sb.append(line);
-      }
-    } catch (MalformedURLException mue) {
-      System.out.println("Ouch - a MalformedURLException happened.");
-      mue.printStackTrace();
-      System.exit(1);
-    } catch (IOException ioe) {
-      System.out.println("Oops- an IOException happened.");
-      ioe.printStackTrace();
-      System.exit(1);
-    } finally {
-      try {
-        is.close();
-      } catch (IOException ignored) {}
-    }
-    return sb.toString();
-  }
-
-  private String parseWeekNumber(String html) {
-    String output;
-
-    Pattern replaceWhitespacePattern = Pattern.compile("\\s");
-    Matcher matcher;
-    matcher = replaceWhitespacePattern.matcher(html);
-    output = matcher.replaceAll(" ");
-
-    Pattern removeHTMLTagsPattern = Pattern.compile("]*>");
-    matcher = removeHTMLTagsPattern.matcher(output);
-    output = matcher.replaceAll("");
-
-    Pattern leaveOnlyAlphaNumericCharactersPattern = Pattern.compile("[^0-9a-zA-Z ]");
-    matcher = leaveOnlyAlphaNumericCharactersPattern.matcher(output);
-    output = matcher.replaceAll("");
-
-    List<String> htmlElements = Arrays.asList(output.split(" "));
-
-    // Remove empty elements
-    List<String> nonEmptyHtmlElements = htmlElements.stream()
-        .filter(e -> !e.equals(""))
-        .filter(Objects::nonNull)
-        .collect(Collectors.toList());
-
-    int index = nonEmptyHtmlElements.indexOf("week");
-    String week = nonEmptyHtmlElements.get(index + 1);
-
-    return week;
-  }
-
 }
--- src/main/java/app/crawl/Crawler.java
+++ src/main/java/app/crawl/Crawler.java
@@ -1,45 +1,66 @@
 package app.crawl;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Collections;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public class Crawler implements Runnable {
 
   private static final Logger LOG = Logger.getLogger(Crawler.class.getName());
 
-  /*
-   * TODO for class:
-   * - If any URL on URL-queue, consume it
-   * - Put any sub-links found on queue
-   * - Pass URL content to next HTML-queue
-   */
-
   @Override
   public void run() {
-
+    // What to do with this? Does this class have to implement Runnable?
   }
 
-  public void crawl(Object url) throws IOException {
+  public Set<String> getSubLinks(String url)  {
+    String lowercaseUrl = url.toLowerCase();
 
-    LOG.log(Level.INFO, "Will crawl URL: {0}", url.toString());
+    Document document = null;
+    try {
+      document = Jsoup.connect(lowercaseUrl).get();
+    } catch (IOException e) {
+      LOG.log(Level.SEVERE, "Error parsing the URL: {0}", e);
+    }
 
-    /*URL url = new URL(urlString);
-    BufferedReader in = new BufferedReader(
-        new InputStreamReader(url.openStream()));
+    // Select all <a> elements with an href attribute
+    Elements linkElements = document.select("a[href]");
 
-    StringBuilder sb = new StringBuilder();
-    String inputLine;
-    while ((inputLine = in.readLine()) != null)
-      sb.append(sanitizeHtml(inputLine) + " ");
-    in.close();
+    try {
+      String domain = getDomain(lowercaseUrl);
 
-    List<String> elementList = Arrays.asList(sb.toString().split(" "));
+      return linkElements.stream()
+          .distinct()
+          // Get value of href attribute
+          .map(element -> element.attr("href"))
+          .map(link -> normalize(domain, link))
+          .collect(Collectors.toSet());
 
-    List<String> hyperlinks = elementList.stream()
-        .collect(Collectors.toList());
+    } catch (URISyntaxException e) {
+      LOG.log(Level.SEVERE, "Malformed URL: {0}", e);
+    }
 
-    return elementList;*/
+    return Collections.emptySet();
   }
 
+  private String normalize(String domain, String link) {
+    // TODO
+    return link;
+  }
+
+  private String getDomain(String url) throws URISyntaxException {
+    // http://stackoverflow.com/questions/9607903/get-domain-name-from-given-url
+    URI uri = new URI(url);
+    String domain = uri.getHost();
+    return domain.startsWith("www.") ? domain.substring(4) : domain;
+  }
 }
