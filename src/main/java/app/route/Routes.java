60ca86bef6991a8ed8e5cd549bcff10f457c3510
Simon Egersand <s.egersand@gmail.com>
2017-04-02 15:35:52 +0200

    Add build-and-run task to Makefile

---
+++ src/main/java/app/route/Routes.java
@@ -0,0 +1,18 @@
+package app.route;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Routes {
+
+  @RequestMapping("/")
+  public String home() {
+    return "Home url";
+  }
+
+  @RequestMapping("/custom")
+  public String doesNotMatterForNow() {
+    return "Custom URL";
+  }
+}
