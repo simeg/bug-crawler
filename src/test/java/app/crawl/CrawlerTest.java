21ebd7f325248d7b8a5e871239cf62ddf7695d5f
Simon Egersand <s.egersand@gmail.com>
2017-04-04 14:52:53 +0200

    Make it possible to run tests

---
+++ src/test/java/app/crawl/CrawlerTest.java
@@ -0,0 +1,14 @@
+package app.crawl;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CrawlerTest {
+
+  @Test
+  public void EXAMPLE() throws Exception {
+    assertEquals("EXAMPLE", 1, 1);
+    assertEquals("EXAMPLE", 1, 0);
+  }
+}
