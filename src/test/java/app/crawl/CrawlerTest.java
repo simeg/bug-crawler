b7782aca785fcaaa8cb5778932c8e8f4087e9288
Simon Egersand <s.egersand@gmail.com>
2017-04-05 22:00:42 +0200

    Add testing question

--- src/test/java/app/crawl/CrawlerTest.java
+++ src/test/java/app/crawl/CrawlerTest.java
@@ -1,11 +1,13 @@
 package app.crawl;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class CrawlerTest {
 
+  // QUESTION
+  // Make methods public for testing?
   @Test
   public void EXAMPLE() throws Exception {
     assertEquals("EXAMPLE", 1, 1);
