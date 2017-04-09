4e2dc75eff67fb981b3c8a2820f3fd47c0280b16
Simon Egersand <s.egersand@gmail.com>
2017-04-09 13:46:49 +0200

    Put looping inside submitWorker method instead of outside

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -50,75 +50,74 @@ public class Application {
 
     final ExecutorService executor = Executors.newFixedThreadPool(50);
 
-    for (int i = 0; i < 10; i++) {
-      submitWorker(executor, subLinkQueue, urlToCrawl -> {
-        LOG.info("Starting crawl thread with name: {}", Thread.currentThread().getName());
+    submitWorkerNTimes(10, executor, subLinkQueue, urlToCrawl -> {
+      LOG.info("Starting crawl thread with name: {}", Thread.currentThread().getName());
 
-        if (!isValidUrl(urlToCrawl)) {
-          LOG.info(
-              "{}: Consumed URL is invalid - aborting: {}",
-              Thread.currentThread().getName(), urlToCrawl);
-          return;
-        }
+      if (!isValidUrl(urlToCrawl)) {
+        LOG.info(
+            "{}: Consumed URL is invalid - aborting: {}",
+            Thread.currentThread().getName(), urlToCrawl);
+        return;
+      }
 
-        final Set<String> subLinks = new Crawler().getSubLinks(urlToCrawl);
+      final Set<String> subLinks = new Crawler().getSubLinks(urlToCrawl);
 
-        // URL is crawled and ready to be analyzed
-        crawledLinkQueue.add(urlToCrawl);
+      // URL is crawled and ready to be analyzed
+      crawledLinkQueue.add(urlToCrawl);
 
-        if (subLinks.size() > 0) {
-          // Add sub-links back on URL queue
-          subLinkQueue.addAll(subLinks);
+      if (subLinks.size() > 0) {
+        // Add sub-links back on URL queue
+        subLinkQueue.addAll(subLinks);
 
-          LOG.info(
-              "{}: Found {} sub-links: {}",
-              Thread.currentThread().getName(), String.valueOf(subLinks.size()), subLinks);
-        } else {
-          LOG.info(
-              "{}: No sub-links found for: {}",
-              Thread.currentThread().getName(), urlToCrawl);
-        }
-      });
-    }
+        LOG.info(
+            "{}: Found {} sub-links: {}",
+            Thread.currentThread().getName(), String.valueOf(subLinks.size()), subLinks);
+      } else {
+        LOG.info(
+            "{}: No sub-links found for: {}",
+            Thread.currentThread().getName(), urlToCrawl);
+      }
+    });
 
-    for (int i = 0; i < 10; i++) {
-      submitWorker(executor, crawledLinkQueue, urlToAnalyze -> {
-        if (urlToAnalyze != null) {
-          LOG.info("Starting analyze thread with name: {}", Thread.currentThread().getName());
+    submitWorkerNTimes(10, executor, crawledLinkQueue, urlToAnalyze -> {
+      if (urlToAnalyze != null) {
+        LOG.info("Starting analyze thread with name: {}", Thread.currentThread().getName());
 
-          // Does nothing right now
-          new Analyzer().analyze(urlToAnalyze);
-        }
-      });
-    }
+        // Does nothing right now
+        new Analyzer().analyze(urlToAnalyze);
+      }
+    });
   }
 
-  private <T> void submitWorker(
+  private <T> void submitWorkerNTimes(
+      final int times,
       ExecutorService executor,
       PersistentQueue<T> queue,
       Consumer<T> jobToDo) {
-    executor.submit(() -> {
-      final String oldName = Thread.currentThread().getName();
-      // TODO: Set appropriate thread name
-      Thread.currentThread().setName("hello");
+    for (int i = 0; i < times; i++) {
+      executor.submit(() -> {
+        final String oldName = Thread.currentThread().getName();
+        // TODO: Set appropriate thread name
+        Thread.currentThread().setName("hello");
 
-      while (true) {
-        try {
-          T urlToAnalyze = queue.poll(10, TimeUnit.SECONDS);
+        while (true) {
+          try {
+            T urlToAnalyze = queue.poll(10, TimeUnit.SECONDS);
 
-          jobToDo.accept(urlToAnalyze);
+            jobToDo.accept(urlToAnalyze);
 
-        } catch (InterruptedException e) {
-          Thread.currentThread().interrupt();
-          LOG.error(
-              "{}: Polling was interrupted: {}",
-              Thread.currentThread().getName(), e.toString());
-          break;
+          } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            LOG.error(
+                "{}: Polling was interrupted: {}",
+                Thread.currentThread().getName(), e.toString());
+            break;
+          }
         }
-      }
 
-      Thread.currentThread().setName(oldName);
-    });
+        Thread.currentThread().setName(oldName);
+      });
+    }
   }
 
   private boolean isValidUrl(String url) {
