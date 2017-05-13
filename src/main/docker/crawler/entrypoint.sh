93f5583622049fdbbd9041158a5595aba67b9bf7
Simon Egersand <s.egersand@gmail.com>
2017-05-13 17:29:27 +0200

    Have Crawler wait for DB container before init

---
+++ src/main/docker/crawler/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+function wait_for_db_container {
+  until psql -h db -U postgres -l > /dev/null 2>&1; do
+    echo "Postgres is down, waiting.."
+    sleep 1
+  done
+
+  echo "Postgres is up"
+}
+
+function start_java_app {
+  java -Djava.security.egd=file:/dev/./urandom -jar /app.jar
+}
+
+wait_for_db_container
+start_java_app
