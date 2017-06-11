7643378fdf865552c0c3e18f77570eba86f8858c
Simon Egersand <s.egersand@gmail.com>
2017-06-12 00:01:14 +0200

    Finish domain handling functions for UrlNumInc
    The functions are also tested. Now all that's missing is making the
    actual requests and seeing if there's any difference in the incremented
    response.

--- src/main/java/app/plugin/UrlNumberIncrement.java
+++ src/main/java/app/plugin/UrlNumberIncrement.java
@@ -6,6 +6,8 @@ import app.util.Utilities;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -27,18 +29,26 @@ public class UrlNumberIncrement implements Plugin {
             "number, this plugin will increment " +
             "that number and request that page. " +
             "If the requested page is not a 404 it's " +
-            "saved as a vulnerability";
+            "saved as a vulnerability.";
   }
 
   @Override
   public Set<Bug> inspect(String url) {
-    if (hasSubPage(url) && subPageHasNumber(url)) {
-      final String subPage = getSubPage(url);
-      final String touchedSubPage = increment(subPage);
+    if (hasSubPage(url) && hasNumber(getSubPage(url))) {
+      // For a set number of times
+      //    Get incremented URL
+      //    Request incremented URL
+      //    Inspect content
     }
 
-
     return null;
+  }
+
+  public static String getIncrementedUrl(String url) {
+    final String subPage = getSubPage(url);
+    final String incrementedSubPage = increment(subPage);
+    final String domain = Utilities.getDomain(url);
+    return String.format("http://www.%s%s", domain, incrementedSubPage);
   }
 
   public static String increment(String subPage) {
@@ -54,24 +64,42 @@ public class UrlNumberIncrement implements Plugin {
     return result.toString();
   }
 
-  public static String getSubPage(String url) {
-    return null;
-  }
-
-  public static boolean subPageHasNumber(String url) {
-    return true;
-  }
-
   public static boolean hasSubPage(String url) {
     final String domain = Utilities.getDomain(url);
 
     if (domain == null) {
-      LOG.warn("Parsing domain from URL={} gave a null response", url);
+      LOG.warn("Parsing domain from URL=[{}] gave a null response", url);
       return false;
     }
 
-    return url.length() > domain.length() && !url.contains("&");
+    final String subPage = getSubPage(url);
 
+    if (subPage.length() <= 1) {
+      return false;
+    } else if (subPage.indexOf("?") == 0) {
+      return false;
+    }
+
+    return true;
   }
 
+  public static String getSubPage(String url) {
+    try {
+      final URI uri = new URI(url);
+      final String query = uri.getQuery();
+      final String path = uri.getPath();
+
+      if (path.equals("") && query != null) {
+        return "?" + query;
+      }
+
+      return path + (query == null ? "" : "?" + query);
+    } catch (URISyntaxException e) {
+      throw new RuntimeException("Unable to parse URL", e);
+    }
+  }
+
+  public static boolean hasNumber(String url) {
+    return url.matches(".*\\d+.*");
+  }
 }
