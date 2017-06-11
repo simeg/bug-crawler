ab417574c4012c6ea8f98c6142b119e06ab50754
Simon Egersand <s.egersand@gmail.com>
2017-06-11 22:51:40 +0200

    Add more complex test cases for Utilities.getDomain

--- src/test/java/app/util/UtilitiesTest.java
+++ src/test/java/app/util/UtilitiesTest.java
@@ -7,10 +7,10 @@ public class UtilitiesTest {
 
   @Test
   public void testIsValidUrlFormats() throws Exception {
-    final String validHttp = "http://www.arbitrary-domain.com/arbitray/";
-    final String validHttpTwoSlashes = "http://www.arbitrary-domain.com//arbitray/";
-    final String validNoWww = "http://arbitrary-domain.com";
-    final String validHttpWww = "http://www.arbitrary-domain.com";
+    final String validHttp = "http://www.arb-domain.com/arb/";
+    final String validHttpTwoSlashes = "http://www.arb-domain.com//arb/";
+    final String validNoWww = "http://arb-domain.com";
+    final String validHttpWww = "http://www.arb-domain.com";
 
     Assert.assertTrue(Utilities.isValidUrl(validHttp));
     Assert.assertTrue(Utilities.isValidUrl(validHttpTwoSlashes));
@@ -20,10 +20,10 @@ public class UtilitiesTest {
     final String missingDomain = "http://.com";
     Assert.assertFalse(Utilities.isValidUrl(missingDomain));
 
-    final String malformedUrl1 = "http:/www.arbitrary-domain.com";
-    final String malformedUrl2 = "http:www.arbitrary-domain.com";
-    final String malformedUrl3 = "httpwww.arbitrary-domain.com";
-    final String malformedUrl4 = "http:///www.arbitrary-domain.com";
+    final String malformedUrl1 = "http:/www.arb-domain.com";
+    final String malformedUrl2 = "http:www.arb-domain.com";
+    final String malformedUrl3 = "httpwww.arb-domain.com";
+    final String malformedUrl4 = "http:///www.arb-domain.com";
 
     Assert.assertFalse(Utilities.isValidUrl(malformedUrl1));
     Assert.assertFalse(Utilities.isValidUrl(malformedUrl2));
@@ -33,13 +33,13 @@ public class UtilitiesTest {
 
   @Test
   public void testIsValidUrlProtocols() throws Exception {
-    final String validHttpUrl = "http://www.arbitrary-domain.com";
-    final String validHttpsUrl = "https://www.arbitrary-domain.com";
+    final String validHttpUrl = "http://www.arb-domain.com";
+    final String validHttpsUrl = "https://www.arb-domain.com";
     Assert.assertTrue(Utilities.isValidUrl(validHttpUrl));
     Assert.assertTrue(Utilities.isValidUrl(validHttpsUrl));
 
-    final String invalidProtocol1 = "ftp://www.arbitrary-domain.com";
-    final String invalidProtocol2 = "pop://www.arbitrary-domain.com";
+    final String invalidProtocol1 = "ftp://www.arb-domain.com";
+    final String invalidProtocol2 = "pop://www.arb-domain.com";
 
     Assert.assertFalse(Utilities.isValidUrl(invalidProtocol1));
     Assert.assertFalse(Utilities.isValidUrl(invalidProtocol2));
@@ -47,28 +47,36 @@ public class UtilitiesTest {
 
   @Test
   public void testNormalizeProtocol() throws Exception {
-    final String slimDomain = "arbitrary-domain.com";
-    final String domain = "www.arbitrary-domain.com";
-    final String domainHttp = "http://www.arbitrary-domain.com";
-    final String domainHttps = "https://www.arbitrary-domain.com";
+    final String slimDomain = "arb-domain.com";
+    final String domain = "www.arb-domain.com";
+    final String domainHttp = "http://www.arb-domain.com";
+    final String domainHttps = "https://www.arb-domain.com";
 
-    Assert.assertEquals(Utilities.normalizeProtocol(slimDomain), "http://arbitrary-domain.com");
-    Assert.assertEquals(Utilities.normalizeProtocol(domain), "http://www.arbitrary-domain.com");
-    Assert.assertEquals(Utilities.normalizeProtocol(domainHttp), "http://www.arbitrary-domain.com");
-    Assert.assertEquals(Utilities.normalizeProtocol(domainHttps), "https://www.arbitrary-domain.com");
+    Assert.assertEquals(Utilities.normalizeProtocol(slimDomain), "http://arb-domain.com");
+    Assert.assertEquals(Utilities.normalizeProtocol(domain), "http://www.arb-domain.com");
+    Assert.assertEquals(Utilities.normalizeProtocol(domainHttp), "http://www.arb-domain.com");
+    Assert.assertEquals(Utilities.normalizeProtocol(domainHttps), "https://www.arb-domain.com");
   }
 
   @Test
   public void testGetDomain() throws Exception {
     final String domain = "http://specific-domain.com";
     final String domainWww = "http://www.specific-domain.com";
-    final String domainWithTrail = "http://www.specific-domain.com/arbitrary-value/arbitrary-value/";
+    final String domainWwwSlashEnd = "http://www.specific-domain.com/";
+    final String domainWithTrail = "http://www.specific-domain.com/arb-value/arb-value/";
     final String domainWithParams =
-        "http://www.specific-domain.com?arbitraryParam1=arbitraryValue1&arbitraryParam2=arbitraryValue2";
+        "http://www.specific-domain.com?arbParam1=arbValue1&arbParam2=arbValue2";
+    final String domainWithTrailAndParams =
+        "http://www.specific-domain.com/arb-value/arb-value?arbParam1=arbValue1&arbParam2=arbValue2";
+    final String domainWithNestedTrailAndParams =
+        "http://www.specific-domain.com/arb-value?arbParam=arbValue/arb-value?arbParam1=arbValue1&arbParam2=arbValue2";
 
     Assert.assertEquals("specific-domain.com", Utilities.getDomain(domain));
     Assert.assertEquals("specific-domain.com", Utilities.getDomain(domainWww));
+    Assert.assertEquals("specific-domain.com", Utilities.getDomain(domainWwwSlashEnd));
     Assert.assertEquals("specific-domain.com", Utilities.getDomain(domainWithTrail));
     Assert.assertEquals("specific-domain.com", Utilities.getDomain(domainWithParams));
+    Assert.assertEquals("specific-domain.com", Utilities.getDomain(domainWithTrailAndParams));
+    Assert.assertEquals("specific-domain.com", Utilities.getDomain(domainWithNestedTrailAndParams));
   }
 }
